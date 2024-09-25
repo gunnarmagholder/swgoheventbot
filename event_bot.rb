428189b50ac2ed1b8532f3aa796dfe88b21c1fa7
Gunnar Magholder <gmagholder@gmail.com>
2024-09-25 12:58:44 +0200

    Initial Commit

---
+++ event_bot.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'icalendar'
+require 'open-uri'
+
+# .ics Datei laden
+ics_file = URI.open('https://swgohevents.com/ical')
+
+# Kalender parsen
+calendars = Icalendar::Calendar.parse(ics_file)
+
+# Alle Events durchlaufen
+calendars.each do |calendar|
+  calendar.events.each do |event|
+    puts "Event: #{event.summary}"
+    puts "Startzeit: #{event.dtstart}"
+    puts "Endzeit: #{event.dtend}"
+    puts "Beschreibung: #{event.description}"
+  end
+end
